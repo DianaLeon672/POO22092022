d2259fae9d26bcededfc75a85ec91b7fcfdbc175
dlaur <dianaleon44@aragon.unam.mx>
2022-05-06 16:19:50 -0500

    clase o5/05/2022

--- clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
+++ clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
@@ -4,6 +4,7 @@
  */
 package javabasico2209;
 
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -112,14 +113,83 @@ public class JavaBasico2209 {
           lista[3] = new Alumno("88888", 2, 8.0f);
           lista[4] = new Alumno("66666", 2, 6.0f);
           
-          for (int i = 0; i < lista.length; i++) {
-            Alumno alumno = lista[i];
-            System.out.println( alumno.evaluarDesempenio() );
+                  /*lenght << Longitud del arreglo*/
+        for (int i = 0; i < lista.length; i++) {
+            Alumno tmp = lista[i];
+            System.out.println(tmp.evaluarDesempenio()); //FORI + TAB
+        tmp.estudiar(30);
         }
-          System.out.println("Con for each...");
-          for (Alumno alumno : lista){
-             System.out.println( alumno.evaluarDesempenio() ); 
-          }
+        /*Otra forma de usar el For*/
+        System.out.println("----------------------------");
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println(lista[i].evaluarDesempenio()); //FORI + TAB
+        }
+        System.out.println("------------------------------------");
+        
+        /*For each es un For especial, introducido en la version 2 de java, esa version fue 
+        la que sufrio cambios muy grandes java
+        EL FOR EACH fue introducido para simplificar los For con arreglos <FOR + TAB>*/
+        System.out.println("---------------------------------------------------------");
+        System.out.println("CON FOR EACH...");
+        for (Alumno alumno : lista) {
+            System.out.println(alumno.evaluarDesempenio());
+        }
+        
+        System.out.println("----------ARRAY LIST-------");
+        ArrayList<Alumno> grupo2209= new ArrayList<>();
+        grupo2209.add(new Alumno("99999", 2, 9.0f));
+        grupo2209.add(new Alumno("88888", 2, 8.0f));
+        grupo2209.add(new Alumno("77777", 2, 7.0f));
+        grupo2209.add(new Alumno("66666", 2, 6.0f));
+        grupo2209.add(new Alumno("55555", 2, 5.0f));
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+        }
+        
+        /**/
+        System.out.println("------------Add con indice---------------");
+        grupo2209.add(2, new Alumno("81111", 3, 9.9f));
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+        }
+        System.out.println("-------Devuelve un elemento--------");
+        Alumno tmp = grupo2209.get(3); //NC= 77777
+        System.out.println("Alumno de index = 1: "+tmp);
+        
+        System.out.println("-------Remover o eliminar--------");
+        System.out.println("Eliminar el index 3 ");
+        Alumno tmp2 = grupo2209.remove(3);
+        System.out.println("Elemento sacado = "+ tmp2) ;
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+        }
+        
+        
+        System.out.println("-----Remplazar el i-esimo elemento-----------");
+        Alumno tmp3 = grupo2209.set(0, new Alumno("444444", 4, 4.0f));
+        System.out.println("El sacado es= "+tmp3);
+        
+        for (Alumno alumno : grupo2209) {
+            System.out.println(alumno);
+        }
+        
+        /*Excepciones*/
+       
+        /*trycatch * TAB*/
+        try {
+            System.out.println("----------Excepciones--------");
+            System.out.println(grupo2209.get(20));
+        } catch (Exception e) {
+            System.out.println("Error.. revisa los indices");
+        }
+
+        System.out.println("Fin del programa ");
+
     }
+        
     
 }
+//Un atributo de clase o metodo de clase(elementos miembros) son identificadores que almacenan un valor constante o un comportamiento constante igual para todo los objetos de la misma.
+//Al ser contante no es necesario crear una instancia(objeto), para acceder a ellos
